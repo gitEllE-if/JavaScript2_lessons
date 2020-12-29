d8e7956e717f5fec86a2d57d8aceb34f59d35212
gitEllE_if <elvira_lina@mail.ru>
2020-12-29 16:29:36 +0300

    lesson4

--- textReplace/textReplace.js
+++ textReplace/textReplace.js
@@ -1,8 +1,14 @@
 'use strict';
 
-let leftDivEl = document.querySelector('.leftDiv');
-let rightDivEl = document.querySelector('.rightDiv');
+let leftDivEl = document.querySelector('.leftDiv');     //для исходного текста
+let rightDivEl = document.querySelector('.rightDiv');   //для замененного текста
 
-leftDivEl.innerHTML += strOrigin;
+leftDivEl.innerHTML += strOrigin; // выводим исходный текст
+
+// Задание 1: замена всех кавычек
+//rightDivEl.innerHTML += strOrigin.replace(/\'/g, '"');
+
+
+// Задание 2: Замена кавычек на границах предложения с прямой речью
 strOrigin = strOrigin.replace(/( )\'/g, '$1"');
 rightDivEl.innerHTML += strOrigin.replace(/([\.\?])\'/g, '$1"');
