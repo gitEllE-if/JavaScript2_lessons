b6ed4f88347723c175c71744dc4ca8270720b114
gitEllE_if <elvira_lina@mail.ru>
2021-01-11 11:06:13 +0300

    lesson4: formValid with OOP

--- formValid/formValid.js
+++ formValid/formValid.js
@@ -1,41 +1,74 @@
 'use strict';
 
-let formEl = document.querySelector('form');
-let inNameEl = document.getElementById('inName');
-let inPhoneEl = document.getElementById('inPhone');
-let inMailEl = document.getElementById('inMail');
-let inTextEl = document.getElementById('inText');
+class Validator {
+    constructor(form) {
+        this.patterns = {
+            inName: /^[A-Z,a-z,А-Я,а-я,ё]{2,}$/i,     // /^[a-zа-яё]+$/i,
+            inPhone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,     // '^([+]7[(])[0-9]{3}[)][0-9]{3}-[0-9]{4}$'
+            inMail: /^[\w._-]+@\w+\.[a-z]{2,4}$/i,    // '^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$'
+            inText: /[\w\d]+/i
+        };
+        this.errors = {
+            inName: 'Required format: name (not less than 2 letters)',
+            inPhone: 'Required format: +7(000)000-0000',
+            inMail: 'Required format: mymail@mail.ru, my.mail@mail.ru, my-mail@mail.ru',
+            inText: 'Required format: any text',
+        };
+        this.errorClass = 'errorIn';
+        this.form = form;
+        this.valid = false;
+        this._validateForm();
+    }
+    validate(regexp, value) {
+        regexp.test(value)
+    }
 
-formEl.addEventListener('submit', function (event) {
-    if (!isValid(inNameEl.value, '([A-Z,a-z,А-Я,а-я,ё]){2,}')) {
-        setAlert(inNameEl);
-        event.preventDefault();
+    _validateForm() {
+        let errors = [...document.getElementById(this.form).querySelectorAll(`.${this.errorClass}`)];
+        for (let error of errors) {
+            error.remove();
+        }
+        let formFields = [...document.getElementById(this.form).getElementsByClassName('inputValid')];
+        for (let field of formFields) {
+            this._validate(field);
+        }
+        if (![...document.getElementById(this.form).querySelectorAll('.invalid')].length) {
+            this.valid = true;
+        }
     }
-    if (!isValid(inPhoneEl.value, '^([+]7[(])[0-9]{3}[)][0-9]{3}-[0-9]{4}$')) {
-        setAlert(inPhoneEl);
-        event.preventDefault();
+    _validate(field) {
+        if (this.patterns[field.name]) {
+            if (!this.patterns[field.name].test(field.value)) {
+                field.classList.add('invalid');
+                this._addErrorMsg(field);
+                this._watchField(field);
+            }
+        }
     }
-    if (!isValid(inMailEl.value, '^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$')) {
-        setAlert(inMailEl);
-        event.preventDefault();
-    }
-    if (inTextEl.value === '') {
-        setAlert(inTextEl);
-        event.preventDefault();
-    }
-});
 
-function isValid(str, regexpStr) {
-    let regexp = new RegExp(regexpStr);
-    return regexp.test(str);
-}
+    _addErrorMsg(field) {
+        let error = `<div class="${this.errorClass}">
+                        invalid input!<br>${this.errors[field.name]}
+                    </div> `;
+        field.insertAdjacentHTML('afterend', error);
+    }
 
-function setAlert(elem) {
-    elem.style.borderColor = 'var(--alertColor)';
-    if (!document.querySelector(`div[data-name=${elem.getAttribute('name')}]`)) {
-        elem.insertAdjacentHTML('afterend',
-            `<div class=errorIn data-name=${elem.getAttribute('name')}>
-            invalid input!<br>Required format: ${elem.getAttribute('placeholder')}
-        </div>`);
+    _watchField(field) {
+        field.addEventListener('input', () => {
+            let error = field.parentNode.querySelector(`.${this.errorClass}`);
+            if (this.patterns[field.name].test(field.value)) {
+                field.classList.remove('invalid');
+                field.classList.add('valid');
+                if (error) {
+                    error.remove();
+                }
+            } else {
+                field.classList.remove('valid');
+                field.classList.add('invalid');
+                if (!error) {
+                    this._addErrorMsg(field);
+                }
+            }
+        })
     }
 }
